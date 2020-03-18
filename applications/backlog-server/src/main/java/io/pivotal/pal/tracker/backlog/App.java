c5be093be3be44b7427ffeac23da8d9239d75480
Santanu-Pivotal <santanu.bhar@cognizant.com>
2020-03-18 10:02:13 +0530

    9th Assignment

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -1,17 +1,19 @@
 package io.pivotal.pal.tracker.backlog;
 
+import java.util.TimeZone;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
 
-import java.util.TimeZone;
-
 
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.backlog", "io.pivotal.pal.tracker.restsupport"})
+@EnableEurekaClient
 public class App {
 
     public static void main(String[] args) {
